92606b0049908bdc3522372ebf4c75ff92a4556b
vincent <vincentmazet42@gmail.com>
2015-11-24 14:29:14 +0100

    modif exo 5 + javadoc

--- src/main/java/com/mycompany/parcautomobile/Visiteur.java
+++ src/main/java/com/mycompany/parcautomobile/Visiteur.java
@@ -23,10 +23,19 @@ public class Visiteur implements java.io.Serializable {
     private static BeanItemContainer<Visiteur> personnes = new BeanItemContainer<>(Visiteur.class);
     private static ArrayList<Visiteur> lp = new ArrayList<Visiteur>();
     
+    /**
+     *Constructeur vide - xml
+     */
     public Visiteur(){
         
     }
 
+    /**
+     *constructeur nv visiteur
+     * @param id
+     * @param nom
+     * @param prenom
+     */
     public Visiteur(int id, String nom, String prenom) {
         this.id = id;
         this.nom = nom;
@@ -35,44 +44,82 @@ public class Visiteur implements java.io.Serializable {
         this.lp.add(this);
     }
 
+    /**
+     *getter id
+     * @return
+     */
     public int getID() {
         return id;
     }
 
+    /**
+     *setter id
+     * @param id
+     */
     public void setID(int id) {
         this.id = id;
     }
 
+    /**
+     *getter nom
+     * @return
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     *setter nom
+     * @param nom
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     *getter nom
+     * @return
+     */
     public String getPrenom() {
         return prenom;
     }
 
+    /**
+     *setter prenom
+     * @param prenom
+     */
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
 
+    /**
+     *Bean get personnes
+     * @return
+     */
     public static BeanItemContainer<Visiteur> getPersonnes() {
         return personnes;
     }
     
+    /**
+     *get array visiteur
+     * @return
+     */
     public static ArrayList<Visiteur> getLPersonnes() {
         return lp;
     }
     
+    /**
+     *setter liste personnes
+     * @param v
+     */
     public static void setLPersonnes(ArrayList<Visiteur> v){
         lp=v;
     }
     
-    
-
+    /**
+     *fct add vehicule pr visiteur
+     * @param v
+     */
     public void addVehicule(Vehicule v) {
         leidVehicule = v.getId();
         if (idVehicule == "0") {
@@ -87,6 +134,10 @@ public class Visiteur implements java.io.Serializable {
 
     }
 
+    /**
+     *getter vehicule utilisé
+     * @return
+     */
     public String getVehiculeUtilise() {
         return idVehicule;
     }
