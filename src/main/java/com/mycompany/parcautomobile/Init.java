b0ea03ace517127dbfc8e9664d393b49d9ab3c4a
vincent <vincentmazet42@gmail.com>
2015-12-04 10:51:20 +0100

    qqs ajouts

--- src/main/java/com/mycompany/parcautomobile/Init.java
+++ src/main/java/com/mycompany/parcautomobile/Init.java
@@ -43,43 +43,48 @@ public class Init {
 
     private static Init uniqueInstance;
 
-
     private Init() {
         XmlDoc doc = new XmlDoc();
 
-        Visiteur vincent = new Visiteur(1, "Mazet", "Vincent");
-        Visiteur marlon = new Visiteur(2, "Chatte-illon", "Marlon");
-        Visiteur alexy = new Visiteur(3, "hivercruysse", "Alexy");
-        
         Type infirmier = new Type("Infirmier");
         String i = infirmier.getLibelle();
-        Visiteur guillaume = new Visiteur(4, "Grellet", "guigui",i);
-        
-        Type pediatre = new Type("Docteur","Pediatre");
+        Type pediatre = new Type("Docteur", "Pediatre");
         String p = pediatre.getLibelle();
-        Visiteur yannick = new Visiteur(5, "Moreau", "yannou",p);
+
+        Visiteur vincent = new Visiteur(1, "Mazet", "Vincent");
+        Visiteur marlon = new Visiteur(2, "Chattillon", "Marlon");
+        Visiteur alexy = new Visiteur(3, "vercruysse", "Alexy");
+        Visiteur guillaume = new Visiteur(4, "Grellet", "Guillaume", i);
+        Visiteur yannick = new Visiteur(5, "Moreau", "Yannick", p);
 
         Vehicule vehicule1 = new Vehicule(1, "Renault", "Clio", 10000);
         Vehicule vehicule2 = new Vehicule(2, "Audi", "A8", 12500);
         Vehicule vehicule3 = new Vehicule(3, "Toyota", "Yaris", 15005);
-        Vehicule vehicule4 = new Vehicule(4, "Renault", "titine", 99999);
+        Vehicule vehicule4 = new Vehicule(4, "Renault", "Megane", 12500);
         Vehicule vehicule5 = new Vehicule(5, "Peugeot", "406", 8765);
+        Vehicule vehicule6 = new Vehicule(6, "Fiat", "Punto", 8500);
+        Vehicule vehicule7 = new Vehicule(7, "Citroen", "c3", 9500);
+        Vehicule vehicule8 = new Vehicule(8, "Ford", "Fiesta", 8850);
+
+        vincent.setTypeVisiteur(p);
+        marlon.setTypeVisiteur(p);
+        alexy.setTypeVisiteur(i);
 
         alexy.addVehicule(vehicule4);
-        alexy.addVehicule(vehicule2);
-        alexy.addVehicule(vehicule1);
         marlon.addVehicule(vehicule1);
         marlon.addVehicule(vehicule2);
-        
-        
+        guillaume.addVehicule(vehicule6);
+        vincent.addVehicule(vehicule7);
+        yannick.addVehicule(vehicule8);
 
         doc.encodXML(Visiteur.getLPersonnes());
         doc.decodXML();
-        
+
     }
 
     /**
-     *lancement instance programme
+     * lancement instance programme
+     *
      * @return
      */
     public static Init getInstance() {
